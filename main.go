e0b98db586ab6156fb49dc595186ccffd8ee4d90
magoma394 <mg4683601@gmail.com>
2026-09-18 10:41:36 +0300

    refactor: introduce Reminder struct and Execute method for modular reminders

--- main.go
+++ main.go
@@ -3,16 +3,32 @@ import(
 	"time"
 	"fmt"
 )
-func main() {
-	fmt.Println("Magoma Buddy is running...")
-	time.Sleep(10 * time.Second)
-	fmt.Println("💧 Water check!")
-	fmt.Println("Did you drink water? [y/n]")
+type Reminder struct {
+	Name string
+	Question string
+}
+
+func (r Reminder) Execute(){
+	fmt.Printf("Magoma Buddy is running for [%s]...\n", r.Name)
+	time.Sleep(2*time.Second)
+
+	fmt.Printf("\n💧 %s check!\n", r.Name)
+	fmt.Println(r.Question)
+
 	var ans string
 	fmt.Scanln(&ans)
-	if ans == "y" || ans == "yes"{
-		fmt.Println("Nice, Stay Hydrated!")
+
+	if ans == "y" || ans == "yes" {
+		fmt.Println("Nice, stay hydrated!")
 	} else {
 		fmt.Println("Kidding!! Go get some hydration right now!")
 	}
+}
+
+func main() {
+	waterReminder := Reminder{
+		Name: "Water",
+		Question: "Did you drink water? [y,n]",
+	}
+	waterReminder.Execute()
 }
